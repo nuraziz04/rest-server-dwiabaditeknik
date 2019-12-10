812e6152d80e7d896441a851ded5c794a251431c
nuraziz04 <nuraziz1994@gmail.com>
2019-12-10 13:48:36 +0700

    api user access menu

--- application/controllers/api/Menu.php
+++ application/controllers/api/Menu.php
@@ -19,14 +19,14 @@ class Menu extends REST_Controller
 	{
 		$roleId = $this->get('roleId');
 		$menuId = $this->get('menuId');
+		$namaMenu = $this->get('namaMenu');
 
-		if($menuId === null){
-			$menu = $this->menu->getMenu($roleId, $menuId);
+		if($roleId === null and $menuId === null and $namaMenu === null){
+			$menu = $this->menu->getMenu($roleId, $menuId, $namaMenu);
 		} else {
-			$menu = $this->menu->getMenu($roleId, $menuId);
-		}
+			$menu = $this->menu->getMenu($roleId, $menuId, $namaMenu);
+		} 
 		
-
 		if($menu){
 			$this->response([
 	            'status' => '00',
@@ -37,6 +37,31 @@ class Menu extends REST_Controller
 	            'status' => '404',
 	            'data' => $menu
 	        ], REST_Controller::HTTP_NOT_FOUND);
+		}
+	}
+
+	public function index_delete()
+	{
+		$id = $this->delete('id');
+
+		if($id === null){
+			$this->response([
+	            'status' => false,
+	            'message' => 'provide an id'
+	        ], REST_Controller::HTTP_BAD_REQUEST);
+		} else {
+			if($this->menu->deleteMenu($id) > 0){
+				$this->response([
+		            'status' => true,
+		            'id' => $id,
+		            'message' => 'Delete'
+	        	], REST_Controller::HTTP_OK);
+			} else {
+				$this->response([
+		            'status' => false,
+		            'message' => 'id not found'
+	        	], REST_Controller::HTTP_BAD_REQUEST);
+			}
 		}
 	}
 
--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -2,9 +2,12 @@
 
 class Menu_model extends CI_Model
 {
-	public function getMenu($roleId, $menuId)
+	public function getMenu($roleId, $menuId, $namaMenu)
 	{
-		if($menuId === null) {
+
+		if($roleId === null and $menuId === null and $namaMenu === null){
+			return $this->db->get('user_menu')->result_array();
+		} else if($menuId === null and $namaMenu === null) {
 		    $queryMenu = "select a.id, a.id_menu
 		                  FROM user_menu a 
 		                  JOIN user_access_menu b
@@ -13,13 +16,28 @@ class Menu_model extends CI_Model
 		                  ORDER BY b.menu_id ASC
 		                ";
 		    return $this->db->query($queryMenu)->result_array();
-		} else {
+		} else if($roleId === null and $namaMenu === null){
 			$querySubMenu = "select * from user_sub_menu a
 							 join user_menu b
 							 on a.menu_id = b.id
 							 where a.menu_id = $menuId and a.is_active = 1
 			";
 			return $this->db->query($querySubMenu)->result_array();
+		} else if($roleId === null and $menuId === null) {
+			return $this->db->get_where('user_menu', ['id_menu' => $namaMenu])->result_array();
+		} else {
+			$userAccess = $this->db->get_where('user_access_menu', [
+				'role_id' => $roleId,
+				'menu_id' => $menuId
+			]);
+
+			return $userAccess->result_array();
 		}
+	}
+
+	public function deleteMenu($id)
+	{
+		$this->db->delete('user_menu', ['id' => $id]);
+		return $this->db->affected_rows();
 	}
 }
